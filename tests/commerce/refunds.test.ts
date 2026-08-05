2f04b4ffe86bc951f6fb60d7e982a0697d9db2e4
Matt Shaw <matt@mttshw.com>
2026-08-05 08:00:49 +0100

    fix test fail

--- tests/commerce/refunds.test.ts
+++ tests/commerce/refunds.test.ts
@@ -10,6 +10,7 @@ function order(overrides: Partial<OrderRow>): OrderRow {
     payment_provider: 'stripe', payment_reference: 'cs_123',
     shipping_rate_id: null, shipping_title: null, tax_amount: 0,
     pickup_address: null, pickup_instructions: null, fulfilment_date: null, fulfilment_window: null,
+    due_date: null, accounting_ref: null,
     tracking_number: null, tracking_url: null, shipped_at: null,
     reminder_sent_at: null,
     created_at: '', updated_at: '',
